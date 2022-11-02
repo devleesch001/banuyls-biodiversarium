40ee6b3f3e3ede33d65e64b96528d33f13d1aa20
adrian.maisueche <adrian.maisueche@imerir.com>
2022-11-02 17:04:48 +0100

    Affichage d'image sur site

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TestImage from "./components/resize_img";
 import './index.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    Application Aquarium
+    <TestImage/>
   </React.StrictMode>
 );
 
