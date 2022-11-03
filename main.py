8ae98d8bfe6f0010f5f95447592478306f0ad4a6
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-11-03 11:27:19 +0100

    add favicon and route

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+API_VERSION = "v1"
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse("favicon.png")
+
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World!"}
+async def home():
+    return {"message": "API for fish recognition! Check documentation on /docs ."}
+
+
+@app.post(f"/{API_VERSION}/object-detection/with-image/")
+async def postObjectDetectionWithImage(file: UploadFile = File(...)):
+    return {f"{UploadFile.file.name}"}
+
+
+@app.post(f"/{API_VERSION}/object-detection/")
+async def postObjectDetection(file: UploadFile = File(...)):
+    return {f"{UploadFile.file.name}"}
 
 
 if __name__ == "__main__":
