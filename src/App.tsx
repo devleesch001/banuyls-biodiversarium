dfb92c9e13c6b260f70ba23e325bf6b7ab001e47
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-11-08 16:32:51 +0100

    Revert "first commit"
    This reverts commit a1b8c0227a422c273d4e1f2e1d1f25e739dfec39.
    
    reimplmeent app and consort

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'jquery/dist/jquery.min.js'
+import "bootstrap/dist/js/bootstrap.js";
+
+import {DrawerAppBar, Camera, FishInformation} from './Component';
+
+function App() {
+    return (
+        <>
+            {/* PARTIE 1 */}
+            <DrawerAppBar/>
+
+            {/* PARTIE 2: Caméra */}
+            <Camera/>
+
+            {/* PARTIE 3: Informations poisson */}
+            <FishInformation/>
+        </>
+    );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'jquery/dist/jquery.min.js'
-import "bootstrap/dist/js/bootstrap.js";
-
-import { DrawerAppBar, Camera, FishInformation  } from './Component';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>    
-    {/* PARTIE 1 */}
-    <DrawerAppBar/>
-
-    {/* PARTIE 2: Caméra */}
-    <Camera/>
-
-    {/* PARTIE 3: Informations poisson */}
-    <FishInformation/>
-
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
