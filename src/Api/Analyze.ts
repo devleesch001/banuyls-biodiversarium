5e9efbd683ece1adfd0cf0a30a1a7fec7a0ea8df
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-11-17 21:53:31 +0100

    use env variable for url api

--- src/Api/Analyze.ts
+++ src/Api/Analyze.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import { SERVER_URL } from '../config';
+import { API_URL } from '../config';
 
 export const test = () => {
-    return axios.get(`${SERVER_URL}/api`);
+    return axios.get(`${API_URL}/api`);
 };
 
 /**
  * @param image base64
  */
 export const analyze = (image: string) => {
-    return axios.post(`${SERVER_URL}/api/mobile/analyze`, {
+    return axios.post(`${API_URL}/api/mobile/analyze`, {
         content: image,
     });
 };
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const SERVER_URL = '';
+export const API_URL = process.env.REACT_APP_API_URL;
