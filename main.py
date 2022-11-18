50bb6925a21724c420fdf09cf4d86d53fb9ed61d
Sébastien BLANCHARD <sebastien.blanchard@imerir.com>
2022-11-18 09:35:13 +0100

    Ajout d'un fichier de config pour paramétrer les chemins des poids, des labels, et de la taille des images utilisées pour l'ia

--- main.py
+++ main.py
@@ -15,8 +15,11 @@ app = FastAPI(docs_url="/ia/docs", redoc_url=None)
 
 API_VERSION = "v1"
 
+with open("config.json","r") as f:
+    config = json.loads(f.read())
 
-inferer_glob = Inferer(None, "weights/best_ckpt.pt", 0, "fishDataset/data.yaml", [416,416], False)
+# inferer_glob = Inferer(None, "weights/best_ckpt.pt", 0, "fishDataset/data.yaml", [416,416], False)
+inferer_glob = Inferer(None, config["weights"], 0, config["labels"], [config["img_size"],config["img_size"]], False)
 inferer_lock = threading.Lock()
 
 @app.get(f"/ia")
@@ -24,64 +27,29 @@ async def root():
     return {"data":{"status":"running"}}
 
 @app.post(f"/ia/{API_VERSION}/image-detection/")
-# @app.post(f"/{API_VERSION}/image-detection/")
 async def postObjectDetection(file: UploadFile = File(...)):
     print(file)
     contents = await file.read()
-    #print(contents)
-    # Inference2
-    #path = "5e295a5_1666084008932-simonbercy.jpg"
-    # inferer = Inferer(contents, "weights/yolov6s.pt", 0, "data/coco.yaml", 640, False)
     if inferer_lock.acquire(False):
         inferer_glob.set_content(contents)
         res = inferer_glob.infer(0.4, 0.45, None, False, 1000, "runs/inference/exp", True, True, False, False, False,use_only_result=True)
         inferer_lock.release()
     else:
-        inferer = Inferer(contents, "weights/best_ckpt.pt", 0, "fishDataset/data.yaml", [416,416], False)
+        inferer = Inferer(None, config["weights"], 0, config["labels"], [config["img_size"],config["img_size"]], False)
         res = inferer.infer(0.4, 0.45, None, False, 1000, "runs/inference/exp", True, True, False, False, False,use_only_result=True)
     print("res",res)
     return {"detection":res}
 
-async def truc(websocket, data):
-    await websocket.send_json(data)
-
-async def some_callback(websocket, data):
-    await truc(websocket, data)
-
-def between_callback(websocket, data):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    loop.run_until_complete(some_callback(websocket, data))
-    loop.close()
-
-
-# @app.websocket(f"/{API_VERSION}/live-detection/")
 @app.websocket(f"/ia/{API_VERSION}/live-detection/")
 async def live_detection(websocket: WebSocket):
     await websocket.accept()
     url = websocket.query_params['target']
     ldt = None
     try:
-        # for i in range(100000):
-        #     await websocket.send_json(json.dumps({"i":i}))
-        # i=0
-        
-        #g = range(1000)
-        #g = inferer.infer_generator(0.4, 0.45, None, False, 1000, "runs/inference/exp", True, True, False, False, False,use_only_result=True)
-        # g = truc(100000)
         ldt = LiveDetectionThread(url)
         ldt.start()
         while True:
-            # t = Thread(target=truc, args=[websocket,json.dumps({"detection":det})])
-            # t.run()
-            
             await websocket.send_json(json.dumps({"detection":ldt.getDet()}))
-            # await websocket.send_json(json.dumps({"detection":det}))
-            # i+=1
-            # if i >1000:
-            #     g.close()
-            #     break
     finally:
         if ldt is not None:
             ldt.stop = True
@@ -90,7 +58,7 @@ class LiveDetectionThread(threading.Thread):
     def __init__(self, url):
         super().__init__()
         # self.inferer = Inferer(url, "weights/yolov6s.pt", 0, "data/coco.yaml", 640, False)
-        self.inferer = Inferer(url, "weights/best_ckpt.pt", 0, "fishDataset/data.yaml", [416,416], False)
+        self.inferer = Inferer(url, config["weights"], 0, config["labels"], [config["img_size"],config["img_size"]], False)
         # Start thread
         self.daemon = True
         self.det = []
@@ -110,7 +78,6 @@ class LiveDetectionThread(threading.Thread):
 
     def run(self):
         g = self.inferer.infer_generator(0.4, 0.45, None, False, 1000, "runs/inference/exp", True, True, False, False, False,use_only_result=True)
-        # g = truc(100000)
         for det in g:
             self.setDet(det)
             if self.stop:
