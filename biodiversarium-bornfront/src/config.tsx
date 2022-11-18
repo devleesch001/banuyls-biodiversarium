10ae41cd8605bb7799a1049176afb613a2922e8e
Alexis DEVLEESCHAUWER <alexis@devleeschauwer.fr>
2022-11-18 09:32:47 +0100

    add env variable

--- biodiversarium-bornfront/src/config.tsx
+++ biodiversarium-bornfront/src/config.tsx
@@ -1,5 +1,7 @@
-export enum config {
-    CAMERA_OUT='http://localhost:8000/out',
-    CAMERA_IN='http://localhost:8000/in',
-    IA_API_TABLET_ANALYZE='http://10.3.1.37:5000/api/tablet/analyze'
-};
+const API_URL: string = process.env.REACT_APP_API_URL ?? ""
+
+export const config = {
+    CAMERA_OUT: 'http://localhost:8000/out',
+    CAMERA_IN: 'http://localhost:8000/in',
+    IA_API_TABLET_ANALYZE: `${API_URL}/api/tablet/analyze`
+}
