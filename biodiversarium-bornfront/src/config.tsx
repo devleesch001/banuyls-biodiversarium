86458bf16c761f4dfb1beda90fdcd924bf65bf1d
Douraid-BEN-HASSEN <douraid.benhassen@gmail.com>
2022-11-18 14:01:58 +0100

    fix config

--- biodiversarium-bornfront/src/config.tsx
+++ biodiversarium-bornfront/src/config.tsx
@@ -1,7 +1,10 @@
 const API_URL: string = process.env.REACT_APP_API_URL ?? "http://10.3.1.37"
 
+export enum camera {
+    CAMERA_OUT='http://localhost:8000/out',
+    CAMERA_IN='http://localhost:8000/in',
+}
+
 export const config = {
-    CAMERA_OUT: 'http://localhost:8000/out',
-    CAMERA_IN: 'http://localhost:8000/in',
     IA_API_TABLET_ANALYZE: `${API_URL}/api/tablet/analyze`
 }
